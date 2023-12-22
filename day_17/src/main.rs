b8963421ad10eac4d18defe8e950acfe66170615
Alfredo <icealfredo123@gmail.com>
2023-12-21 18:06:54 -0600

    day 17 part one solved

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -1,7 +1,8 @@
 mod priority_queue;
 use std::{collections::HashMap, hash::Hash};
 
-use priority_queue::{PriorityQueue, Node, Point};
+use priority_queue::{PriorityQueue, Node, Direction, Point};
+use priority_queue::Direction::{*};
 
 struct Graph {
     map: Vec<Vec<Node>>,
@@ -27,67 +28,45 @@ impl Graph {
         Self { map }
     }
 
-    fn get_height(&self) -> usize {
-        self.map.len()
-    }
+    fn neighbors(&self, node: &Node, direction: Direction) -> Vec<(&Node, Direction)> {
+        let mut neighbors: Vec<(&Node, Direction)> = Vec::new();
 
-    fn get_width(&self) -> usize {
-        self.map[0].len()
-    }
+        match direction {
+            North(score) | South(score) => {
 
-    fn is_goal(&self, node: &Node) -> bool {
-        (node.loc.x as usize == self.get_width() - 1) 
-            &&
-        (node.loc.y as usize == self.get_height() - 1) 
-    }
+                self.gen_west(&mut neighbors, node, West(2));
+                self.gen_east(&mut neighbors, node, East(2));
 
-    fn within_bounds(&self, x: isize, y: isize) -> bool {
-        ((x >= 0) && (x <= (self.get_width() - 1) as isize))
-            &&
-        ((y >= 0) && (y <= (self.get_height() - 1) as isize))
-    }
-
-    fn neighbors(&self, node: &Node) -> Vec<&Node> {
-        let mut neighbors: Vec<&Node> = Vec::new();
-
-        //Top neighbor
-        //print!("-->Top {:}, ", self.within_bounds(node.loc.x as isize, node.loc.y as isize - 1));
-        if self.within_bounds(node.loc.x as isize, node.loc.y as isize - 1) {
-            neighbors.push(&self.map[(node.loc.y - 1) as usize][node.loc.x as usize]);
-        }
-
-        //Bottom neighbor
-        //print!("Bottom {:}, ", self.within_bounds(node.loc.x as isize, node.loc.y as isize + 1));
-        if self.within_bounds(node.loc.x as isize, node.loc.y as isize + 1) {
-            neighbors.push(&self.map[(node.loc.y + 1) as usize][node.loc.x as usize]);
-        }
-
-        //left neighbor
-        //print!("Left {:}, ", self.within_bounds(node.loc.x as isize - 1, node.loc.y as isize));
-        if self.within_bounds(node.loc.x as isize - 1, node.loc.y as isize) {
-            neighbors.push(&self.map[node.loc.y as usize][(node.loc.x - 1) as usize]);
-        }
-
-        //right neighbor
-        //println!("Right {:}/<--", self.within_bounds(node.loc.x as isize + 1, node.loc.y as isize));
-        if self.within_bounds(node.loc.x as isize + 1, node.loc.y as isize) {
-            neighbors.push(&self.map[node.loc.y as usize][(node.loc.x + 1) as usize]);
-        }
-
-        //println!("Checking neighbors for, {:?}", node);
-        for neighbor in &neighbors{ 
-            //println!("  neighbor, {:?}", neighbor);
+                if score != 0 {
+                    match direction {
+                        North(_) => self.gen_north(&mut neighbors, node, North(score - 1)),
+                        South(_) => self.gen_south(&mut neighbors, node, South(score - 1)),
+                        _ => panic!("Matched west/east inside a north/south"),
+                    }
+                }
+            },
+            East(score) | West(score) => {
+                self.gen_north(&mut neighbors, node, North(2));
+                self.gen_south(&mut neighbors, node, South(2));
+                if score != 0 {
+                    match direction {
+                        East(_) => self.gen_east(&mut neighbors, node, East(score - 1)),
+                        West(_) => self.gen_west(&mut neighbors, node, West(score - 1)),
+                        _ => panic!("Matched north/south inside a east/west"),
+                    }
+                }
+            },
         }
         neighbors
     }  
     
-    fn reconstruct_path(&self, came_from: HashMap<&Node, Option<&Node>>) {
+    fn reconstruct_path(&self, came_from: HashMap<&Node, &Node>) {
         let mut current = &self.map[self.get_height()-1][self.get_width()-1]; 
         let mut path: Vec<&Node> = Vec::new();
 
         while (current.loc.x != 0) || (current.loc.y != 0) {
             path.push(current);
-            current = came_from.get(current).unwrap().unwrap();
+            current = came_from.get(current).unwrap();
         }
         let path: Vec<&Node> = path.into_iter().rev().collect();
         
@@ -96,62 +75,71 @@ impl Graph {
         }
 
     }
-    
-
+    fn get_cost(&self, cost: HashMap<(&Node, Direction), u32>) {
+        
+    }
 
     fn q1(&self) {
-        let mut fringe = PriorityQueue::new();
-        fringe.insert(&self.map[0][0], 0);
-
-        let mut came_from: HashMap<&Node, Option<&Node>> = HashMap::new(); 
-        let mut cost_so_far: HashMap<&Node, u32> = HashMap::new();
-
-        came_from.insert(&self.map[0][0], None); 
-        cost_so_far.insert(&self.map[0][0], 0); 
+        let mut fringe = PriorityQueue::new(
+            (&self.map[0][1], East(2),  self.map[0][1].weight),
+            (&self.map[1][0], South(2), self.map[1][0].weight),
+        );
+        let mut came_from: HashMap<(&Node, Direction), (&Node, Direction)> = HashMap::from([
+             ((&self.map[0][1], East(2)), (&self.map[0][0],North(2))),
+             ((&self.map[1][0], South(2)), (&self.map[0][0], North(2))),
+        ]); 
+        let mut cost_so_far: HashMap<(&Node, Direction), u32> = HashMap::from([
+             ((&self.map[0][1], East(2)), self.map[0][1].weight),
+             ((&self.map[1][0], South(2)), self.map[1][0].weight),
+        ]);
+        let mut total_cost: u32 = 0;
 
         
         while !fringe.is_empty() {
-            let (current, cost_from_current): (&Node, u32) = fringe.pop();
-            //println!("Current node, {:?}, ", current);
+            let (current, direction, cost_from_current): (&Node, Direction, u32) = fringe.pop();
+            println!("Current node, {:?}, direction {:?}", current, direction);
 
 
             if self.is_goal(current){
+                total_cost = cost_from_current;
                 break;
             }
 
-            
-            for next in self.neighbors(current).iter() {
-                //println!("Looking at the neighbor, {:?}, ", next);
-                let new_cost = cost_so_far[current] + next.weight;
-                if !(cost_so_far.contains_key(next)) || (new_cost < *cost_so_far.get(next).unwrap()) {
-                    //println!("       passed");
-                    match cost_so_far.get_mut(next) {
+            for (next, next_direction) in self.neighbors(current, direction).iter() {
+                println!("     Looking at the neighbor, {:?}, direction {:?}", next, next_direction);
+                let new_cost = cost_so_far[&(current, direction)] + next.weight;
+                if !(cost_so_far.contains_key(&((next, *next_direction)))) 
+                     || (new_cost < *cost_so_far.get(&((next, *next_direction))).unwrap()) {
+                    println!("          |");
+                    println!("          |-> passed, new_cost, {:}", new_cost);
+                    match cost_so_far.get_mut(&((next, *next_direction))) {
                         Some(next_node) => {
                             *next_node = new_cost;
                         },
                         None => {
-                            cost_so_far.insert(next, new_cost);
+                            cost_so_far.insert((next, *next_direction), new_cost);
                         }
                     }
-                    fringe.insert(next, new_cost);
-                    match came_from.get_mut(next) {
+                    fringe.insert(next, *next_direction, new_cost);
+                    match came_from.get_mut(&((next, *next_direction))) {
                         Some(next_node) => {
-                            *next_node = Some(current);
+                            *next_node = (current, direction);
                         },
                         None => {
-                            came_from.insert(next, Some(current));
+                            came_from.insert((next, *next_direction), (current, direction));
                         }
                     }
                 }
             }
         }
 
+        println!("==================");
+        println!("==================");
+        println!("==================");
 
-        println!("Dist {:?}", cost_so_far.get(
-                            &self.map[self.get_height()-1][self.get_width()-1]
-            ).unwrap()
-        );
-        self.reconstruct_path(came_from);
+        println!("Q1 Dist {:?}", total_cost)
+
+        //self.reconstruct_path(came_from);
         //for test in came_from.iter() {
         //    println!("{:?}", test);
         //}
@@ -164,7 +152,8 @@ impl Graph {
 
 fn main() {
     //let file = include_str!("../input/sample.txt");
-    let file = include_str!("../input/sample_two.txt");
+    //let file = include_str!("../input/sample_two.txt");
+    let file = include_str!("../input/input.txt");
     let graph = Graph::new(file);
     graph.q1();
 }
--- day_17/src/priority_queue.rs
+++ day_17/src/priority_queue.rs
@@ -1,3 +1,14 @@
+use crate::Graph;
+
+
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
+pub enum Direction {
+    North(u32),
+    South(u32),
+    East(u32),
+    West(u32)
+}
+
 #[derive(Debug, Eq, Hash, PartialEq)]
 pub struct Point {
     pub x: u32,
@@ -23,12 +34,18 @@ impl Node {
 }
 
 pub struct PriorityQueue<'a> {
-    pub queue: Vec<(&'a Node, u32)>,
+    pub queue: Vec<(&'a Node, Direction, u32)>,
 }
 
 impl<'a> PriorityQueue<'a> {    
-    pub fn new() -> Self {
-        Self { queue: Vec::new() }
+    pub fn new(
+            right: (&'a Node, Direction, u32),
+            bottom: (&'a Node, Direction, u32)
+        ) -> Self {
+        let queue: Vec<(&'a Node, Direction, u32)> = vec![right, bottom];
+        let mut temp = Self { queue };
+        temp.sort();
+        temp
     }
     pub fn dump(&self) {
         println!("Current queue: ");
@@ -36,17 +53,72 @@ impl<'a> PriorityQueue<'a> {
             println!("      {:?}", node);
         }
     }
+    fn sort(&mut self) {
+        self.queue.sort_by(|a, b| (a.2).partial_cmp(&b.2).unwrap());
+    }
 
-    pub fn insert(&mut self, node: &'a Node, cost: u32) {
-        self.queue.push((node, cost));
-        self.queue.sort_by(|a, b| (a.1).partial_cmp(&b.1).unwrap());
-        //self.dump();
+    pub fn insert(&mut self, node: &'a Node, dir: Direction, cost: u32) {
+        self.queue.push((node, dir, cost));
+        self.sort();
+        self.dump();
     }
     pub fn is_empty(&self) -> bool {
         self.queue.len() == 0 
     }
-    pub fn pop(&mut self) -> (&'a Node, u32) {
+    pub fn pop(&mut self) -> (&'a Node, Direction, u32) {
         self.queue.remove(0)
     }
+}
 
+impl Graph {
+    pub fn get_height(&self) -> usize {
+        self.map.len()
+    }
+
+    pub fn get_width(&self) -> usize {
+        self.map[0].len()
+    }
+
+    pub fn is_goal(&self, node: &Node) -> bool {
+        (node.loc.x as usize == self.get_width() - 1) 
+            &&
+        (node.loc.y as usize == self.get_height() - 1) 
+    }
+
+    pub fn within_bounds(&self, x: isize, y: isize) -> bool {
+        ((x >= 0) && (x <= (self.get_width() - 1) as isize))
+            &&
+        ((y >= 0) && (y <= (self.get_height() - 1) as isize))
+    }
+
+    pub fn gen_neighbor(&self, x: isize, y: isize) -> Option<&Node> {
+        if self.within_bounds(x, y) {
+            return Some(&self.map[y as usize][x as usize]);
+        }
+        None
+    }
+
+    pub fn gen_west<'a>(&'a self, neighbors: &mut Vec<(&'a Node, Direction)>, node: &Node, direction: Direction) {
+        if let Some(found) = self.gen_neighbor(node.loc.x as isize - 1, node.loc.y as isize) {
+            neighbors.push((found, direction));
+        }
+    }
+
+    pub fn gen_east<'a>(&'a self, neighbors: &mut Vec<(&'a Node, Direction)>, node: &Node, direction: Direction) {
+        if let Some(found) = self.gen_neighbor(node.loc.x as isize + 1, node.loc.y as isize) {
+            neighbors.push((found, direction));
+        }
+    }
+
+    pub fn gen_north<'a>(&'a self, neighbors: &mut Vec<(&'a Node, Direction)>, node: &Node, direction: Direction) {
+        if let Some(found) = self.gen_neighbor(node.loc.x as isize, node.loc.y as isize - 1) {
+            neighbors.push((found, direction));
+        }
+    }
+
+    pub fn gen_south<'a>(&'a self, neighbors: &mut Vec<(&'a Node, Direction)>, node: &Node, direction: Direction) {
+        if let Some(found) = self.gen_neighbor(node.loc.x as isize, node.loc.y as isize + 1) {
+            neighbors.push((found, direction));
+        }
+    }
 }
