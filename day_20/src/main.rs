49e0173e307d2800fd6217a3f5aa2d49f2bbc665
Alfredo <icealfredo123@gmail.com>
2023-12-26 17:11:01 -0600

    started day 20 part two

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -93,7 +93,7 @@ impl<'a> Conjuction<'a> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Module<'a> {
     mod_type: ModuleType,
     flip_info: Option<FlipFlop<'a>>,
@@ -259,6 +259,35 @@ impl<'a> Module<'a> {
     }
 }
 
+struct ExtraInfo {
+    dest: String,
+    high_pulse_index: Vec<(usize, Pulse)>,
+    count: usize,
+}
+
+impl ExtraInfo {
+    fn new(dest: String) -> Self {
+        Self {
+            dest,
+            high_pulse_index: Vec::new(),
+            count: 0,
+        }
+    }
+    fn dump(&self) {
+        println!("dest: {:}", self.dest);
+        for i in 1..self.high_pulse_index.len() {
+            println!("index: {:?}, pulse {:?}, diff {:}", 
+                     self.high_pulse_index[i].0,
+                     self.high_pulse_index[i].1,
+                     self.high_pulse_index[i].0 - self.high_pulse_index[i - 1].0,
+                     )
+        }
+        println!("count: {:}", self.count);
+    }
+
+}
+
+
 #[derive(Debug)]
 struct ModuleConfiguration<'a> {
     broadcast_connections: Vec<&'a str>,
@@ -268,6 +297,15 @@ struct ModuleConfiguration<'a> {
 }
 
 impl<'a> ModuleConfiguration<'a> {
+    fn new_from_modules(modules: Vec<Module<'a>>, start_loc: &'a str) -> Self {
+        Self {
+            modules,
+            broadcast_connections: vec![start_loc],
+            low_pulse_count: 0,
+            high_pulse_count: 0,
+        }
+    }
+
     fn new(file: &'a str) -> Self {
         let mut map: HashMap<&'a str, Vec<&'a str>> = HashMap::new();
         let mut broadcast_connections: Vec<&'a str> = Vec::new();
@@ -319,7 +357,7 @@ impl<'a> ModuleConfiguration<'a> {
         println!("{:}", self.high_pulse_count);
     }
 
-    fn broadcast(&mut self) {
+    fn broadcast(&mut self, info: &mut Option<ExtraInfo>) {
         let mut actions: Vec<Action> = Vec::new();
         let temp = Module::new_broadcast(self.broadcast_connections.clone());
         actions.append(&mut temp.gen_actions());
@@ -336,6 +374,20 @@ impl<'a> ModuleConfiguration<'a> {
                     self.low_pulse_count += 1;
                 }
             }
+
+            
+            if info.is_some() {
+
+
+                info.as_mut().unwrap().count += 1;
+                let temp = info.as_ref().unwrap().count;
+
+                if info.as_mut().unwrap().dest == front.dest && front.pulse == Pulse::Low {
+                    info.as_mut().unwrap().high_pulse_index.push((temp, Pulse::High));
+                }
+            }
+            
+
             let found_module = self.modules.iter_mut().find(|module| {
                 if let Some(found) = module.flip_info {
                     found.name == front.dest
@@ -357,8 +409,68 @@ impl<'a> ModuleConfiguration<'a> {
         }
     }
 
+    fn get_sub_graphs(&self) -> Vec<ModuleConfiguration> {
+        let mut module_clone = self.modules.clone();
+
+        //Find node that points to rx
+        let pos = module_clone.iter().position(|module| {
+            module.connections.iter().find(|name| {
+                **name == "rx"
+            }).is_some()
+        }).unwrap();
+
+        
+        let pointer_to_rx = module_clone.remove(pos);
+
+        let mut sub_graphs: Vec<ModuleConfiguration> = Vec::new();
+
+        for start in &self.broadcast_connections {
+            let mut sub_graph: Vec<Module> = Vec::new();
+            let mut connections_to_find: Vec<&str> = Vec::new();
+
+
+            connections_to_find.push(start);
+
+            while connections_to_find.len() != 0 {
+                let connection_name = connections_to_find.remove(0);
+
+                match module_clone.iter().position(|module| {
+                    if let Some(flip) = &module.flip_info {
+                        flip.name == connection_name
+                    }
+                    else if let Some(conj) = &module.conj_info {
+                        conj.name == connection_name
+                    }
+                    else{
+                        false
+                    }
+                }) {
+                    Some(found_connection_pos) => {
+                        let node_connected_to = module_clone.remove(found_connection_pos);
+                        node_connected_to.connections.iter().for_each(|connection_name| {
+                            connections_to_find.push(connection_name);
+                        });
+                        sub_graph.push(node_connected_to);
+                    }
+                    //Probably found in a prev iteration
+                    None => {
+                        continue;
+                    }
+                }
+            }
+            sub_graphs.push(ModuleConfiguration::new_from_modules(sub_graph, start));
+        }
+
+        sub_graphs
+    }
+
+    fn monitor_high_pulse_cycle(&mut self, dest: String){
+
+    }
+
+
     fn q1(&mut self) -> u64 {
-        (0..1000).for_each(|_| self.broadcast());
+        (0..1000).for_each(|_| self.broadcast(&mut None));
         self.low_pulse_count * self.high_pulse_count
     }
 }
@@ -368,5 +480,38 @@ fn main() {
     let file = include_str!("../input/input.txt");
 
     let mut module_configuration = ModuleConfiguration::new(file);
-    println!("{:}", module_configuration.q1());
+    module_configuration.get_sub_graphs();
+    //println!("{:}", module_configuration.q1());
 }
+
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+    
+    #[test]
+    fn monitor_high_pulse_cycle() {
+        let file = include_str!("../input/input.txt");
+        let mut module_configuration = ModuleConfiguration::new(file);
+        let mut sub_graphs: Vec<ModuleConfiguration> = module_configuration.get_sub_graphs();
+
+        println!("Size {:}", sub_graphs.len());
+        for mut sub_graph in sub_graphs {
+            let mut extra = Some(ExtraInfo::new(sub_graph.broadcast_connections[0].to_string()));
+
+            (0..30).for_each(|_| {
+                sub_graph.broadcast(&mut extra)
+            });
+
+            extra.unwrap().dump();
+        }
+    }
+}
+
+
+
+
+
+
+
+
